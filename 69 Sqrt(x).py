48560f716ed17d3ab807f98c678609a17c4f3aec
unknown <vishveshpathak1999@gmail.com>
2026-01-02 15:25:58 +0530

    02-01-2026 third commit

---
+++ 69 Sqrt(x).py
@@ -0,0 +1,19 @@
+"""
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+You must not use any built-in exponent function or operator.
+    For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+Example 1:
+Input: x = 4
+Output: 2
+Explanation: The square root of 4 is 2, so we return 2.
+"""
+
+#solution:
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        i = 1
+        while i*i<x:
+            i = i*2
+        while i*i>x:
+            i = i-1
+        return i
