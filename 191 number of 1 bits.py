9b0192b255a3af13e3537b871f67695bbdd7b3e7
unknown <vishveshpathak1999@gmail.com>
2026-01-06 16:58:52 +0530

    06-01-2026 first commit

---
+++ 191 number of 1 bits.py
@@ -0,0 +1,15 @@
+"""
+Given a positive integer n, write a function that returns the number of
+in its binary representation (also known as the Hamming weight).
+Example 1:
+Input: n = 11
+Output: 3
+Explanation:
+The input binary string 1011 has a total of three set bits.
+"""
+#solution:
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        s = bin(n)[2:]
+        return s.count('1')
